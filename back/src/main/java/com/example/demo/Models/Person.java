ff9783b382ac3ad4c7a3f3eb15e2855875f5fa22
Eya HajAyed <eyahadjayed@gmail.com>
2023-09-16 17:37:28 +0100

    update

--- back/src/main/java/com/example/demo/Models/Person.java
+++ back/src/main/java/com/example/demo/Models/Person.java
@@ -34,7 +34,43 @@ public class Person {
                 '}';
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
